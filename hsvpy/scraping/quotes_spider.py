7a25ef81557325b00159bc86f045d1253f6d6344
Pat Viafore <patviafore@gmail.com>
2017-07-24 22:48:49 -0500

    Updating presentation

---
+++ hsvpy/scraping/quotes_spider.py
@@ -0,0 +1,30 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        
+        for quote in response.css('div.quote'):
+            print({
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('span small::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            })
+
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+})
+
+process.crawl(QuotesSpider) 
+process.start() # the script will block here until the crawling is finished
